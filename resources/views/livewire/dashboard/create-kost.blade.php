ababceefa94561f4895c58b7e0f174a4aed1dc1d
Limzy71 <muhikhsanjr05@gmail.com>
2026-07-30 23:12:33 +0700

    style(create-kost): shrink Kecamatan column to sm:w-52 to better fit longest district name

--- resources/views/livewire/dashboard/create-kost.blade.php
+++ resources/views/livewire/dashboard/create-kost.blade.php
@@ -476,7 +476,7 @@
 
                 <div class="flex flex-col sm:flex-row gap-6 items-start">
                     <!-- Dropdown Kecamatan -->
-                    <div class="space-y-2 w-full sm:w-60 shrink-0">
+                    <div class="space-y-2 w-full sm:w-52 shrink-0">
                         <label for="district" class="block text-xs font-black uppercase tracking-wider text-black">
                             Kecamatan <span class="text-rose-600">*</span>
                         </label>

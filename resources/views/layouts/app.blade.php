b8a77ad85825a9535efa9511bfb6165d080ce4b6
Limzy71 <muhikhsanjr05@gmail.com>
2026-08-14 14:07:54 +0700

    Update default page title tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'KostBandung.web.id - Cari Kost Hyper-Local' }}</title>
+    <title>{{ $title ?? 'KostBandung - kost Khusus Kota Bandung' }}</title>
     {!! $meta ?? '' !!}
     {!! $head ?? '' !!}
     <link rel="preconnect" href="https://fonts.googleapis.com">

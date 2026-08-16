8e027376f39b288165367d46307c26bb18db263b
Limzy71 <muhikhsanjr05@gmail.com>
2026-08-16 20:20:38 +0700

    chore: update default meta title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'KostBandung - kost Khusus Kota Bandung' }}</title>
+    <title>{{ $title ?? 'KostBandung - Cari Kost Khusus Kota Bandung' }}</title>
     {!! $meta ?? '' !!}
     {!! $head ?? '' !!}
     <link rel="preconnect" href="https://fonts.googleapis.com">
